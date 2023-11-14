407e37ae8c2c51626062a0385b779b81b0cf72dc
Yahia Ibrahim <Yahiasheriif87@gmail.com>
2023-11-14 17:10:08 +0200

    Add project files.

---
+++ Deque.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+
+internal partial class Program
+{
+    class Deque<T>:IEnumerable<T>
+    {
+        private LinkedList<T> _items = new LinkedList<T>();
+
+        public void Add(params T[] values)
+        {
+            foreach (T value in values)
+            {
+                _items.AddLast(value);
+            }
+        }
+        public void AddFront(T item)
+        {
+            _items.AddFirst(item);
+        }
+        public void AddRear(T item)
+        {
+            _items.AddLast(item);
+        }
+        public T RemoveFront()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The deque is empty");
+            }
+            T value = _items.First.Value;
+            _items.RemoveFirst();
+            return value;
+        }
+        public T RemoveRear()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The deque is empty");
+            }
+            T value = _items.Last.Value;
+            _items.RemoveLast();
+            return value;
+        }
+        public T PeekFront()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The deque is empty");
+            }
+            return _items.First.Value;
+        }
+        public T PeekRear()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The deque is empty");
+            }
+            return _items.Last.Value;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+        public int count =>_items.Count;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in _items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => (IEnumerator<T>)GetEnumerator();
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+       Deque<char> deque = new Deque<char>();
+        deque.Add('r','o','g','r','a','m','m','e');
+        deque.AddFront('P');
+        deque.AddRear('r');
+        foreach (char c in deque)
+        {
+            Console.Write(c+" ");
+        }
+
+
+        Console.ReadKey();
+    }
+}
